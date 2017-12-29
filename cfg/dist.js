630de3b7f6817c1899ac8b949c7fef3f1acb0bc0
kangchuang <v_kangchuang@baidu.com>
2017-12-29 14:08:16 +0800

    fix gallery

--- cfg/dist.js
+++ cfg/dist.js
@@ -16,7 +16,7 @@ let config = Object.assign({}, baseConfig, {
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': "production"
+      'process.env.NODE_ENV': '"production"'
     }),
     new BowerWebpackPlugin({
       searchResolveModulesDirectories: false
